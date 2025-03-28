836f79093ed75fbad787a85621c0ecf289009b3b
bin-commits <daotittuan@gmail.com>
2025-03-28 17:22:11 +0700

    add_filewed_bandogonoithat

---
+++ products.js
@@ -0,0 +1,60 @@
+const products = [
+    {
+        "id": 1,
+        "name":" LD01 GHẾ THƯ GIÃN",
+        "price": 200,
+        "image": "images/1.png",
+        "description": "$Expertly được thiết kế bởi Carl Hansen & Søn, chiếc ghế dài — được giới thiệu lần đầu tiên vào năm 1951 và tồn tại lâu dài kể từ đó — có sẵn bằng gỗ sồi hoặc kết hợp giữa gỗ sồi và gỗ, có nguồn gốc từ lâm nghiệp bền vững. Chọn từ bọc ghế và lưng với nhiều tùy chọn da hoặc bằng vải tùy chỉnh."
+    },
+    {
+        "id": 2,
+        "name":" LD02 BÀN TRÀ TULIP",
+        "price": 250,
+        "image": "images/popular1.png",
+        "description": "Bàn trà Tulip - Sự Kết Hợp Hoàn Hảo Giữa Hiện Đại & Thanh Lịch Mang phong cách Mid-Century Modern, chiếc bàn trà Tulip này là lựa chọn lý tưởng cho không gian sống tinh tế. Mặt bàn gỗ cao cấp với màu nâu trầm ấm kết hợp cùng chân bàn kim loại sơn đen bóng, tạo nên sự thanh lịch và tối giản nhưng không kém phần sang trọng."
+    },
+    {
+        "id": 3,
+        "name":" LD03 TỦ GỖ ĐẦU GIƯỜNG",
+        "price": 290,
+        "image": "images/products1.png",
+        "description": "Tủ đầu giường gỗ tối màu.Thanh lịch & Tiện dụng - Tủ đầu giường với thiết kế tối giản, gam màu trầm sang trọng cùng tay nắm kim loại ánh vàng tinh tế. Hai ngăn kéo rộng rãi giúp bạn lưu trữ đồ dùng một cách gọn gàng."
+    },
+    {
+        "id": 4,
+        "name":" LD04 GHẾ SOFA TRÒN",
+        "price": 200,
+        "image": "images/about.png",
+        "description": "Ghế hình cầu hiện đại. Độc đáo & Sang trọng - Chiếc ghế với thiết kế hình cầu ấn tượng, lớp vỏ ngoài trắng bóng cùng nội thất nệm êm ái màu cam nổi bật Một sản phẩm hoàn hảo cho không gian hiện đại, giúp bạn thư giãn tối đa."
+    },
+    {
+        "id": 5,
+        "name":" LD05 BÀN TRÀ GỖ NHỎ XINH",
+        "price": 300,
+        "image": "images/products2.png",
+        "description": "Bàn trà gỗ nhỏ xinh. Nhẹ nhàng & Tinh tế - Bàn trà tròn với chân gỗ tự nhiên kết hợp mặt bàn trắng trang nhã. Thiết kế ba tầng tiện lợi giúp bạn vừa có không gian bày trí đẹp mắt, vừa lưu trữ sách báo hoặc đồ trang trí."
+    },
+    {
+        "id": 6,
+        "name":" LD06 TỦ GỖ 6 NGĂN KÉO",
+        "price": 200,
+        "image": "images/products4.png",
+        "description": "Tủ gỗ 6 ngăn kéo. Đa năng & Đẳng cấp - Chiếc tủ gỗ rộng rãi với sáu ngăn kéo lớn, được chế tác từ gỗ tự nhiên mang đến sự bền bỉ và vẻ đẹp tinh tế. Tay nắm kim loại đen tạo điểm nhấn hiện đại, phù hợp với mọi không gian sống."
+    },
+    {
+        "id": 7,
+        "name":" LD07 BÀN GHẾ GỖ",
+        "price": 200,
+        "image": "images/products5.png",
+        "description": "Bàn ăn 6 ghế. Sang trọng & Tiện nghi - Bộ bàn ăn 6 ghế với thiết kế hiện đại, gam màu trung tính dễ dàng phối hợp với nội thất xung quanh. Chất liệu gỗ chắc chắn, mang đến trải nghiệm ấm cúng và thoải mái cho bữa ăn gia đình."
+    },
+    {
+        "id": 8,
+        "name":" LD08 GHẾ SOFA PHÒNG KHÁCH",
+        "price": 200,
+        "image": "images/products8.png",
+        "description": "Sofa vải màu vàng phong cách Mid-century. Nổi bật & Ấm áp – Chiếc sofa màu vàng tươi sáng, thiết kế theo phong cách Mid-century với chân gỗ thanh lịch. Nệm dày êm ái giúp bạn có những giây phút thư giãn tuyệt vời ngay tại phòng khách."
+    }
+
+];
+export default products;
